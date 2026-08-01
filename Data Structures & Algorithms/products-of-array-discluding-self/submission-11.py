15d4efd48c6c0fc4c4d9f5b41a942ffc70e2d1e6
Saad Agaria <saadagaria.7658@gmail.com>
2026-07-31 20:28:37 -0400

    Add: products-of-array-discluding-self - submission-11

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-11.py
@@ -0,0 +1,21 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+       
+       #This one this the optimal solution, before we stored the prefix and suffix in a different list but this time will store it in the output list itself. We will make 2 passes, the first one will get the prefixes in the ouput list and the second one we will multiply the suffixes with the prefixes
+
+       output = [1] * len(nums)
+       temp = 1
+
+       for i in range(0, len(nums)):
+        if i==0:
+            output[i] = 1
+        else:
+            output[i] = nums[i-1] * output[i-1]
+
+       for i in range(len(nums) - 1, -1, -1):
+        if not(i == len(nums) - 1):
+            temp *= nums[i+1]
+            output[i] *= temp 
+
+       return output
+
