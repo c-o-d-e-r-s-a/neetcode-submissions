eb11f5f8dae30f31c8338aab5282fcbebe83ca01
Saad Agaria <saadagaria.7658@gmail.com>
2026-08-05 00:31:55 -0400

    Add: longest-consecutive-sequence - submission-20

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-20.py
@@ -0,0 +1,40 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+   
+        #O(n) solution we only start a sequence if there is no element that is less than 1 in the dictionary
+       
+        if len(nums) == 0:
+         return 0
+
+        myDict = {}
+        seq = 1
+        temp = 0
+
+
+        for num in nums:
+            if num in myDict:
+                myDict[num] += 1
+            else:
+                myDict[num] = 1
+
+        myArr = list(myDict.keys())
+
+        i = 0
+
+        while i < len(myArr):
+            if myArr[i] - 1 not in myDict:
+                count = myArr[i]
+                while count + 1 in myDict:
+                    seq += 1
+                    count += 1
+                if(seq > temp):
+                    temp = seq
+            i += 1
+            seq = 1
+            
+
+        return temp
+
+                
+            
+        
