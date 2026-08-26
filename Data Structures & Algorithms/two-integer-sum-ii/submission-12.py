4834812874bdb025b87d981b6817eda5a77d32aa
Saad Agaria <saadagaria.7658@gmail.com>
2026-08-26 16:50:29 -0400

    Add: two-integer-sum-ii - submission-12

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-12.py
@@ -0,0 +1,18 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+
+        #Using Two pointers
+        #Key here is if sum is too much bring the right to left
+        #If some is too little bring the left to right
+
+        i = 0
+        j = len(numbers) - 1
+
+        while i < j:
+
+            if numbers[i] + numbers[j] < target:
+                i += 1
+            elif numbers[i] + numbers[j] > target:
+                j -= 1
+            else:
+                return [i + 1, j + 1]
