d5f2b220126b0983b3d9bdc87718c40587eb618c
Saad Agaria <saadagaria.7658@gmail.com>
2026-08-31 13:10:46 -0400

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.py
@@ -0,0 +1,34 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+
+        #Start with 2 pointers one at the left and one at the right
+        #Lets choose the first and the last bar to form a container
+        #Index 0 and Index n-1. 
+        #Width = n-1, height = min(heights[0],heights[n-1])
+
+        i,j,area = 0, len(heights)-1, 0
+        area = (j - i) * min(heights[i], heights[j])
+
+        while i < j:
+
+            temp = heights[i]
+            
+            if heights[i] < heights[j]:
+                while not heights[i] > temp and i < j:
+                    i += 1
+
+            elif heights[j] < heights[i]:
+
+                temp = heights[j]
+
+                while not heights[j] > temp and j > i:
+                    j -= 1
+
+            else:
+                i += 1
+
+            area = max(area,(j - i) * min(heights[i], heights[j]))
+
+        return area
+
+            
