1ad97e38a0ae48943ba78cee6c303fcb537ad048
Saad Agaria <saadagaria.7658@gmail.com>
2026-09-03 13:50:32 -0400

    Add: trapping-rain-water - submission-4

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-4.py
@@ -0,0 +1,31 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+
+        #This is the optimal solution: Time O(n)
+        # We will use 2 pointers, one will start on the left and one will start at the extreme right
+        #We will store the max value of the left in the variable left and the max value of the right in the variable right
+
+        left, right = 0, len(height) - 1
+        left_max, right_max = 0, 0
+        boxes = 0
+
+        while left < right:
+            if height[left] < height[right]:
+                if height[left] >= left_max:
+                    left_max = height[left]
+                else:
+                    boxes += left_max - height[left]
+                left += 1
+            else:
+                if height[right] >= right_max:
+                    right_max = height[right]
+                else:
+                    boxes += right_max - height[right]
+                right -= 1
+
+        return boxes
+            
+            
+             
+
+        
